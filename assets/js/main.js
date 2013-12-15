fd61d0d808de3b5f1dd5bba0d556c14d2a1d3b50
cody92 <codydoc92@gmail.com>
2013-12-15 11:20:11 +0200

    adaugare grafic(inceput)

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,8 +26,6 @@ $(function () {
         $(this).children('span').children('i').toggleClass('icon-angle-down');
     });
 
-    $('#menu').metisMenu();
-
 });
 
 function formGeneral() {
@@ -79,59 +77,12 @@ function formGeneral() {
 function formValidation() {
     "use strict";
     /*----------- BEGIN validationEngine CODE -------------------------*/
-    $('#project-submit').validationEngine();
+    $('#project-submit').validationEngine({
+        onValidationComplete: function (form, status) {
+            if (status) {
+                generateNewChart();
+            }
+        }
+    });
     /*----------- END validationEngine CODE -------------------------*/
 };
-(function ($, window, document, undefined) {
-
-    var pluginName = "metisMenu",
-        defaults = {
-            toggle: true
-        };
-
-    function Plugin(element, options) {
-        this.element = element;
-        this.settings = $.extend({}, defaults, options);
-        this._defaults = defaults;
-        this._name = pluginName;
-        this.init();
-    }
-
-    Plugin.prototype = {
-        init: function () {
-
-            var $this = $(this.element),
-                $toggle = this.settings.toggle;
-
-            $this.find('li.active').has('ul').children('ul').addClass('collapse in');
-            $this.find('li').not('.active').has('ul').children('ul').addClass('collapse');
-
-            $this.find('li').has('ul').children('a').on('click', function (e) {
-                e.preventDefault();
-
-                $(this).parent('li').toggleClass('active').children('ul').collapse('toggle');
-
-                if ($toggle) {
-                    $(this).parent('li').siblings().removeClass('active').children('ul.in').collapse('hide');
-                }
-            });
-        }
-    };
-
-    $.fn[pluginName] = function (options) {
-        return this.each(function () {
-            if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
-            }
-        });
-    };
-
-})(jQuery, window, document);
-
-function metisSortable() {
-
-    $('.inner .row').sortable({
-
-    });
-
-}
--- assets/js/sa.js
+++ assets/js/sa.js
@@ -7,7 +7,7 @@ function testSumaProcente(field, rules, i, options) {
     param_1 = param_1 ? param_1 : 0;
     param_2 = param_2 ? param_2 : 0;
     param_3 = param_3 ? param_3 : 0;
-    //alert(field.attr('data-next'));
+
     param_1 = parseInt(param_1);
     param_2 = parseInt(param_2);
     param_3 = parseInt(param_3);
@@ -22,7 +22,7 @@ function verificareTimpMaxim(field, rules, i, options) {
     var camp = field.val();
     timpMax = timpMax ? timpMax : 0;
     camp = camp ? camp : 0;
-    //alert(field.attr('data-next'));
+
     timpMax = parseInt(timpMax);
     camp = parseInt(camp);
     if (timpMax == 0) {
@@ -38,7 +38,7 @@ function verificarePerioadaTimp(field, rules, i, options) {
     var data_prev = field.attr('data-prev');
     var prevTime = $("#timp_" + data_prev).val();
     var time = field.val();
-    //alert(data_prev + " " + prevTime + " " + time);
+
     prevTime = prevTime ? prevTime : 0;
     time = time ? time : 0;
 
@@ -47,4 +47,70 @@ function verificarePerioadaTimp(field, rules, i, options) {
     if (prevTime > time) {
         return 'Aceasta valoare trebuie sa fie mai mare decat: ' + prevTime;
     }
+}
+
+function generateNewChart() {
+    "use strict";
+    var param_1 = $("#parametru_rata_estimari").val();
+    var param_2 = $("#parametru_personal").val();
+    var param_3 = $("#parametru_resurse_materiale").val();
+    var timpStabilit = $("#timp_stabilit").val();
+    var timp1 = $("#timp_1").val();
+    var procent1 = $("#procent_1").val();
+
+    param_1 = param_1 ? param_1 : 0;
+    param_2 = param_2 ? param_2 : 0;
+    param_3 = param_3 ? param_3 : 0;
+    timpStabilit = timpStabilit ? timpStabilit : 0;
+    timp1 = timp1 ? timp1 : 0;
+    procent1 = procent1 ? procent1 : 0;
+
+    param_1 = parseInt(param_1);
+    param_2 = parseInt(param_2);
+    param_3 = parseInt(param_3);
+    timpStabilit = parseInt(timpStabilit);
+    timp1 = parseInt(timp1);
+    procent1 = parseInt(procent1);
+    chart();
+    return false;
+
+}
+
+function chart(label) {
+    "use strict";
+    var d2 = [
+        [0, 3],
+        [1, 8],
+        [2, 5],
+        [3, 13],
+        [4, 1]
+    ];    
+    $.plot($("#chart"), [
+        {
+            data: d2,
+            label: label
+        }
+    ], {
+        clickable: true,
+        hoverable: true,
+        series: {
+            lines: {
+                show: true,
+                fill: true,
+                fillColor: {
+                    colors: [
+                        {
+                            opacity: 0.5
+                        },
+                        {
+                            opacity: 0.15
+                        }
+            ]
+                }
+            },
+            points: {
+                show: true
+            }
+        }
+    });
 }
